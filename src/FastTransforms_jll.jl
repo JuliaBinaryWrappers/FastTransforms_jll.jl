6b765794281c58ae6a8b8535541a8cdf83905a8a
jlbuild <juliabuildbot@gmail.com>
2026-04-07 15:03:38 +0000

    FastTransforms_jll build 0.6.4+0

--- src/FastTransforms_jll.jl
+++ src/FastTransforms_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("FastTransforms")
-JLLWrappers.@generate_main_file("FastTransforms", UUID("34b6f7d7-08f9-5794-9e10-3819e4c7e49a"))
+JLLWrappers.@generate_main_file("FastTransforms", Base.UUID("34b6f7d7-08f9-5794-9e10-3819e4c7e49a"))
 end  # module FastTransforms_jll
